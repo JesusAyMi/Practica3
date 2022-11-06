4af492f070bbb0d716fba6e4f204ef472828248d
JesusAyala <jesusayalamilan2@gmail.com>
2022-11-07 00:42:34 +0100

    punto 18

--- app/src/main/java/net/iessochoa/jesusayala/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/jesusayala/practica3/MainActivity.kt
@@ -2,11 +2,15 @@ package net.iessochoa.jesusayala.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import net.iessochoa.jesusayala.practica3.databinding.ActivityMainBinding
+
+private const val TAG = "mostrar log"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var num: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,32 @@ class MainActivity : AppCompatActivity() {
             num++
             binding.tvNumero.text = num.toString()
         }
+
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        Log.i(TAG, "onPause")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i(TAG, "onStop")
+        super.onStop()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy")
+        super.onDestroy()
     }
 }
